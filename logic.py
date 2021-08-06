6babe916e9755e4fa80a606fd0e9c2548c8dd6a6
Joey Van Lierop <joey.vanlierop@me.com>
2021-08-06 16:47:22 -0600

    feat: avoid hitting self

--- logic.py
+++ logic.py
@@ -27,6 +27,34 @@ def avoid_my_neck(my_head: Dict[str, int], my_body: List[dict], possible_moves: 
     return possible_moves
 
 
+def avoid_my_body(my_head: Dict[str, int], my_body: List[dict], possible_moves: List[str]) -> List[str]:
+    """
+    my_head: Dictionary of x/y coordinates of the Battlesnake head.
+            e.g. {"x": 0, "y": 0}
+    my_body: List of dictionaries of x/y coordinates for every segment of a Battlesnake.
+            e.g. [ {"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0} ]
+    possible_moves: List of strings. Moves to pick from.
+            e.g. ["up", "down", "left", "right"]
+
+    return: The list of remaining possible_moves which don't run into my snakes body
+    """
+    up = {"x": my_head["x"], "y": my_head["y"] + 1}
+    down = {"x": my_head["x"], "y": my_head["y"] - 1}
+    left = {"x": my_head["x"] + 1, "y": my_head["y"]}
+    right = {"x": my_head["x"] - 1, "y": my_head["y"]}
+
+    if left in my_body:                 # Body is left of head
+        possible_moves.remove("left")
+    elif right in my_body:              # Body is right of head
+        possible_moves.remove("right")
+    elif down in my_body:               # Body is below of head
+        possible_moves.remove("down")
+    elif up in my_body:                 # Body is above of head
+        possible_moves.remove("up")
+
+    return possible_moves
+
+
 def avoid_board_edge(my_head: Dict[str, int], board_height: int, board_width: int, possible_moves: List[str]) -> List[str]:
     """
     my_head: Dictionary of x/y coordinates of the Battlesnake head.
@@ -76,6 +104,8 @@ def choose_move(data: dict) -> str:
     # Prevent snake from moving back in on it's own neck
     possible_moves = avoid_my_neck(my_head, my_body, possible_moves)
 
+    # Prevent snake from hitting itself
+    possible_moves = avoid_my_body(my_head, my_body, possible_moves)
     # Prevent snake from moving off the edge of the board
     board_height = data["board"]["height"]
     board_width = data["board"]["width"]
