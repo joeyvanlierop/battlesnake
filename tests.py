4d6bc82a346039ce8e3e82d631e31a2ab948608d
Joey Van Lierop <joey.vanlierop@me.com>
2021-08-06 17:12:46 -0600

    test: test body avoidance

--- tests.py
+++ tests.py
@@ -12,7 +12,7 @@ in the folder where this file exists:
 """
 import unittest
 
-from logic import avoid_my_neck
+from logic import avoid_my_neck, avoid_my_body
 
 
 class AvoidNeckTest(unittest.TestCase):
@@ -86,6 +86,83 @@ class AvoidNeckTest(unittest.TestCase):
 
         # Act
         result_moves = avoid_my_neck(test_head, test_body, possible_moves)
+
+        # Assert
+        self.assertEqual(len(result_moves), 3)
+        self.assertEqual(expected, result_moves)
+
+
+class AvoidBodyTest(unittest.TestCase):
+    def test_avoid_self_all(self):
+        """
+        The possible move set should be all moves.
+
+        In the starter position, a Battlesnake body is 'stacked' in a
+        single place, and thus all directions are valid.
+        """
+        # Arrange
+        test_head = {"x": 5, "y": 5}
+        test_body = [{"x": 5, "y": 5}, {"x": 5, "y": 5}, {"x": 5, "y": 5}]
+        possible_moves = ["up", "down", "left", "right"]
+
+        # Act
+        result_moves = avoid_my_body(test_head, test_body, possible_moves)
+
+        # Assert
+        self.assertEqual(len(result_moves), 4)
+        self.assertEqual(possible_moves, result_moves)
+
+    def test_avoid_body_left(self):
+        # Arrange
+        test_head = {"x": 5, "y": 5}
+        test_body = [{"x": 5, "y": 5}, {"x": 4, "y": 5}, {"x": 3, "y": 5}]
+        possible_moves = ["up", "down", "left", "right"]
+        expected = ["up", "down", "right"]
+
+        # Act
+        result_moves = avoid_my_body(test_head, test_body, possible_moves)
+
+        # Assert
+        self.assertEqual(len(result_moves), 3)
+        self.assertEqual(expected, result_moves)
+
+    def test_avoid_body_right(self):
+        # Arrange
+        test_head = {"x": 5, "y": 5}
+        test_body = [{"x": 5, "y": 5}, {"x": 6, "y": 5}, {"x": 7, "y": 5}]
+        possible_moves = ["up", "down", "left", "right"]
+        expected = ["up", "down", "left"]
+
+        # Act
+        result_moves = avoid_my_body(test_head, test_body, possible_moves)
+
+        # Assert
+        self.assertEqual(len(result_moves), 3)
+        self.assertEqual(expected, result_moves)
+
+    def test_avoid_body_up(self):
+        # Arrange
+        test_head = {"x": 5, "y": 5}
+        test_body = [{"x": 5, "y": 5}, {"x": 5, "y": 6}, {"x": 5, "y": 7}]
+        possible_moves = ["up", "down", "left", "right"]
+        expected = ["down", "left", "right"]
+
+        # Act
+        result_moves = avoid_my_body(test_head, test_body, possible_moves)
+
+        # Assert
+        self.assertEqual(len(result_moves), 3)
+        self.assertEqual(expected, result_moves)
+
+    def test_avoid_body_down(self):
+        # Arrange
+        test_head = {"x": 5, "y": 5}
+        test_body = [{"x": 5, "y": 5}, {"x": 5, "y": 4}, {"x": 5, "y": 3}]
+        possible_moves = ["up", "down", "left", "right"]
+        expected = ["up", "left", "right"]
+
+        # Act
+        result_moves = avoid_my_body(test_head, test_body, possible_moves)
 
         # Assert
         self.assertEqual(len(result_moves), 3)
